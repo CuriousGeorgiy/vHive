70fd920e10dcd2099b2595305d6d89ce6d834d8d
Georgiy Lebedev <lebedev.gk@phystech.edu>
2023-08-31 15:29:23 +0300

    reproducer

--- vanilla_firecracker_snapshots/go.mod
+++ vanilla_firecracker_snapshots/go.mod
@@ -8,14 +8,16 @@ replace (
 	google.golang.org/genproto => google.golang.org/genproto v0.0.0-20200224152610-e50cd9704f63
 )
 
-replace github.com/firecracker-microvm/firecracker-containerd => github.com/curiousgeorgiy/firecracker-containerd v0.0.0-20230827193625-5e898e2012a5
+replace (
+	github.com/firecracker-microvm/firecracker-containerd => github.com/curiousgeorgiy/firecracker-containerd v0.0.0-20230824081423-509353015a9e
+	github.com/firecracker-microvm/firecracker-go-sdk => github.com/curiousgeorgiy/firecracker-go-sdk v1.0.1-0.20230827193418-e5258c49d214
+)
 
 require (
 	github.com/containerd/containerd v1.6.20
-	github.com/containerd/nerdctl v0.18.0
 	github.com/coreos/go-iptables v0.6.0
 	github.com/firecracker-microvm/firecracker-containerd v0.0.0-20230718221715-2a60b1c50228
-	github.com/opencontainers/image-spec v1.1.0-rc2.0.20221005185240-3a7f492d3f1b
+	github.com/firecracker-microvm/firecracker-go-sdk v0.22.1-0.20220427214706-47505a9cf951
 	github.com/pkg/errors v0.9.1
 	github.com/vishvananda/netlink v1.2.1-beta.2
 	github.com/vishvananda/netns v0.0.0-20211101163701-50045581ed74
@@ -24,62 +26,57 @@ require (
 require (
 	github.com/Microsoft/go-winio v0.5.2 // indirect
 	github.com/Microsoft/hcsshim v0.9.8 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/containerd/cgroups v1.0.4 // indirect
-	github.com/containerd/console v1.0.3 // indirect
 	github.com/containerd/continuity v0.3.0 // indirect
 	github.com/containerd/fifo v1.1.0 // indirect
-	github.com/containerd/imgcrypt v1.1.4 // indirect
-	github.com/containerd/stargz-snapshotter v0.11.3 // indirect
 	github.com/containerd/ttrpc v1.1.2 // indirect
 	github.com/containerd/typeurl v1.0.2 // indirect
-	github.com/containers/ocicrypt v1.1.3 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
-	github.com/docker/cli v20.10.14+incompatible // indirect
-	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v20.10.14+incompatible // indirect
-	github.com/docker/docker-credential-helpers v0.6.4 // indirect
+	github.com/containernetworking/cni v1.1.2 // indirect
+	github.com/containernetworking/plugins v1.1.1 // indirect
 	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
-	github.com/docker/go-units v0.4.0 // indirect
+	github.com/go-openapi/analysis v0.21.2 // indirect
+	github.com/go-openapi/errors v0.20.3 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/loads v0.21.1 // indirect
+	github.com/go-openapi/runtime v0.24.0 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-openapi/strfmt v0.21.3 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/go-openapi/validate v0.22.0 // indirect
 	github.com/gogo/googleapis v1.4.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/ipfs/go-cid v0.1.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.15.6 // indirect
-	github.com/klauspost/cpuid/v2 v2.0.6 // indirect
-	github.com/miekg/pkcs11 v1.1.1 // indirect
-	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
-	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/moby/locker v1.0.1 // indirect
 	github.com/moby/sys/mountinfo v0.6.2 // indirect
 	github.com/moby/sys/signal v0.7.0 // indirect
-	github.com/mr-tron/base58 v1.2.0 // indirect
-	github.com/multiformats/go-base32 v0.0.3 // indirect
-	github.com/multiformats/go-base36 v0.1.0 // indirect
-	github.com/multiformats/go-multibase v0.0.3 // indirect
-	github.com/multiformats/go-multihash v0.0.15 // indirect
-	github.com/multiformats/go-varint v0.0.6 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc2.0.20221005185240-3a7f492d3f1b // indirect
 	github.com/opencontainers/runc v1.1.7 // indirect
 	github.com/opencontainers/runtime-spec v1.0.3-0.20220311020903-6969a0a09ab1 // indirect
 	github.com/opencontainers/selinux v1.10.1 // indirect
-	github.com/pelletier/go-toml v1.9.5 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
-	github.com/stefanberger/go-pkcs11uri v0.0.0-20201008174630-78d3cae3a980 // indirect
-	github.com/urfave/cli v1.22.4 // indirect
-	go.mozilla.org/pkcs7 v0.0.0-20200128120323-432b2356ecb1 // indirect
+	go.mongodb.org/mongo-driver v1.10.0 // indirect
 	go.opencensus.io v0.23.0 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/term v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad // indirect
 	google.golang.org/grpc v1.47.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
-	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- vanilla_firecracker_snapshots/taskworkflow.go
+++ vanilla_firecracker_snapshots/taskworkflow.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/cio"
+	"github.com/containerd/containerd/namespaces"
+	"github.com/containerd/containerd/oci"
+	fcclient "github.com/firecracker-microvm/firecracker-containerd/firecracker-control/client"
+	"github.com/firecracker-microvm/firecracker-containerd/proto"
+	"github.com/firecracker-microvm/firecracker-containerd/runtime/firecrackeroci"
+	sdk "github.com/firecracker-microvm/firecracker-go-sdk"
+	"github.com/vhive-serverless/vanilla-firecracker-snapshots/networking"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	"net"
 )
 
 const (
 	containerdAddress      = "/run/firecracker-containerd/containerd.sock"
 	containerdTTRPCAddress = containerdAddress + ".ttrpc"
-	namespaceName          = "vanilla-firecracker-snapshots"
+	containerdNamespace    = "vanilla-firecracker-snapshots"
 	macAddress             = "AA:FC:00:00:00:01"
 	hostDevName            = "tap0"
 	snapshotter            = "devmapper"
@@ -22,11 +29,8 @@ const (
 func main() {
 	var vmID = flag.String("id", "", "virtual machine identifier")
 	var image = flag.String("image", "", "container image name")
-	var revision = flag.String("revision", "", "revision identifier")
-	var snapsBasePath = flag.String("snapshots-base-path", "", "base path for snapshots")
-	var keepalive = flag.Int("keepalive", 3600, "keepalive timeout")
-	var makeSnap = flag.Bool("make-snap", false, "bootstrap and make a snapshot")
 	var bootFromSnap = flag.Bool("boot-from-snap", false, "boot from snapshot")
+	var containerSnapPath = flag.String("container-snap-path", "", "path to container snapshot")
 
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
 	flag.Parse()
@@ -39,125 +43,195 @@ func main() {
 		log.Fatal("Incorrect usage. 'image' needs to be specified when 'boot-snap' is false")
 	}
 
-	if *snapsBasePath == "" {
-		log.Fatal("Incorrect usage. 'snapshots-base' needs to be specified")
+	if *containerSnapPath == "" && *bootFromSnap {
+		log.Fatal("Incorrect usage. 'image' needs to be specified when 'boot-snap' is false")
 	}
 
-	if *revision == "" {
-		log.Fatal("Incorrect usage. 'revision' needs to be specified")
-	}
-
-	if err := taskWorkflow(*vmID, *image, *revision, *snapsBasePath, *keepalive, *makeSnap, *bootFromSnap); err != nil {
+	if err := taskWorkflow(*vmID, *image, *bootFromSnap, *containerSnapPath); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Press the Enter Key to stop anytime")
+	fmt.Scanln()
 }
 
-func taskWorkflow(vmID, image, revision, snapsBasePath string, keepAlive int, makeSnap, bootFromSnap bool) (err error) {
-	log.Println("Creating orchestrator")
-	orch, err := NewOrchestrator(snapshotter, namespaceName, snapsBasePath)
-	if err != nil {
-		return fmt.Errorf("creating orchestrator: %w", err)
-	}
-
+func taskWorkflow(vmID, image string, bootFromSnap bool, containerSnapPath string) (err error) {
 	log.Println("Creating network")
-	if err := orch.networkManager.CreateNetwork(vmID); err != nil {
+	networkManager := networking.NewNetworkManager()
+	if err := networkManager.CreateNetwork(vmID); err != nil {
 		return fmt.Errorf("creating network: %w", err)
 	}
 
 	if !bootFromSnap {
 		log.Println("Bootstrapping VM")
-		err = bootstrapVM(orch, vmID, image)
+		err = bootstrapVM(networkManager, vmID, image)
 		if err != nil {
 			return fmt.Errorf("bootstrapping VM: %w", err)
 		}
-
-		if makeSnap {
-			log.Println("Creating VM snapshot")
-			time.Sleep(3 * time.Second)
-			err = orch.createSnapshot(vmID, revision)
-			if err != nil {
-				return fmt.Errorf("creating VM snapshot: %w", err)
-			}
-		}
 	} else {
-		log.Println("Booting VM from snapshot")
-		err = bootVMFromSnapshot(orch, vmID, revision)
+		err = bootVMfromSnapshot(networkManager, vmID, containerSnapPath)
 		if err != nil {
-			return fmt.Errorf("booting VM from snapshot: %w", err)
+			return fmt.Errorf("boot VM from snapshot: %w", err)
 		}
 	}
-	fmt.Printf("VM available at IP: %s\n", orch.networkManager.GetConfig(vmID).GetCloneIP())
-	time.Sleep(3 * time.Second)
+	fmt.Printf("VM available at IP: %s\n", networkManager.GetConfig(vmID).GetCloneIP())
 
-	SetupCloseHandler(orch, vmID)
-	time.Sleep(time.Duration(keepAlive) * time.Second)
-	log.Println("Tearing down VM")
-	err = tearDownVM(orch, vmID)
-	if err != nil {
-		return fmt.Errorf("tearing down VM: %w", err)
-	}
+	networkManager.RemoveNetwork(vmID)
 
 	return nil
 }
 
-func bootstrapVM(orch *Orchestrator, vmID, imageName string) error {
-	log.Println("Retrieving container image")
-	image, err := orch.getContainerImage(imageName)
+func bootstrapVM(networkManager *networking.NetworkManager, vmID, imageName string) error {
+	log.Println("Creating containerd client")
+	client, err := containerd.New(containerdAddress)
 	if err != nil {
-		return fmt.Errorf("getting container image: %w", err)
+		return fmt.Errorf("creating containerd client: %w", err)
 	}
 
-	log.Println("Creating VM")
-	err = orch.createVM(vmID)
+	log.Println("Creating firecracker client")
+	fcClient, err := fcclient.New(containerdTTRPCAddress)
 	if err != nil {
-		return fmt.Errorf("creating VM: %w", err)
+		return fmt.Errorf("creating firecracker client: %w", err)
 	}
 
-	log.Println("Starting container")
-	err = orch.startContainer(vmID, getSnapKey(vmID), imageName, image)
+	ctx := namespaces.WithNamespace(context.Background(), containerdNamespace)
+
+	log.Printf("Pulling image: %s\n", imageName)
+	image, err := client.Pull(ctx, imageName,
+		containerd.WithPullUnpack,
+		containerd.WithPullSnapshotter(snapshotter),
+	)
 	if err != nil {
-		return fmt.Errorf("starting container: %w", err)
+		return fmt.Errorf("pulling image: %w", err)
 	}
+
+	createVMRequest := &proto.CreateVMRequest{
+		VMID: vmID,
+		// Enabling Go Race Detector makes in-microVM binaries heavy in terms of CPU and memory.
+		MachineCfg: &proto.FirecrackerMachineConfiguration{
+			VcpuCount:  2,
+			MemSizeMib: 2048,
+		},
+		NetworkInterfaces: []*proto.FirecrackerNetworkInterface{{
+			StaticConfig: &proto.StaticNetworkConfiguration{
+				MacAddress:  macAddress,
+				HostDevName: hostDevName,
+				IPConfig: &proto.IPConfiguration{
+					PrimaryAddr: networkManager.GetConfig(vmID).GetContainerCIDR(),
+					GatewayAddr: networkManager.GetConfig(vmID).GetGatewayIP(),
+					Nameservers: []string{"8.8.8.8"},
+				},
+			},
+		}},
+		NetNS: networkManager.GetConfig(vmID).GetNamespacePath(),
+	}
+
+	log.Println("Creating firecracker VM")
+	_, err = fcClient.CreateVM(ctx, createVMRequest)
+	if err != nil {
+		return fmt.Errorf("creating firecracker VM: %w", err)
+	}
+
+	log.Println("Creating new container")
+	ctr, err := client.NewContainer(
+		ctx,
+		getSnapKey(vmID),
+		containerd.WithSnapshotter(snapshotter),
+		containerd.WithNewSnapshot(getSnapKey(vmID), image),
+		containerd.WithNewSpec(
+			oci.WithImageConfig(image),
+			firecrackeroci.WithVMID(vmID),
+			firecrackeroci.WithVMNetwork,
+		),
+		containerd.WithRuntime("aws.firecracker", nil),
+	)
+	if err != nil {
+		return fmt.Errorf("creating new container: %w", err)
+	}
+
+	log.Println("Creating new container task")
+	task, err := ctr.NewTask(ctx, cio.NewCreator(cio.WithStreams(nil, nil, nil)))
+	if err != nil {
+		return fmt.Errorf("creating new container task: %w", err)
+	}
+
+	log.Println("Starting container task")
+	if err := task.Start(ctx); err != nil {
+		return fmt.Errorf("starting container task: %w", err)
+	}
+
+	log.Println("Getting container snapshot mounts")
+	mounts, err := client.SnapshotService(snapshotter).Mounts(ctx, getSnapKey(vmID))
+	if err != nil {
+		return fmt.Errorf("getting container mounts: %w", err)
+	}
+	if len(mounts) != 1 {
+		log.Panic("expected snapshot to only have one mount")
+	}
+	log.Printf("Container snapshot mount: %s \n", mounts[0].Source)
 
 	return nil
-}
-
-func bootVMFromSnapshot(orch *Orchestrator, vmID, revision string) error {
-	log.Println("Booting VM from snapshot")
-	err := orch.bootVMFromSnapshot(vmID, revision)
-	if err != nil {
-		return fmt.Errorf("booting VM from snapshot: %w", err)
-	}
-
-	return nil
-}
-
-func tearDownVM(orch *Orchestrator, vmID string) error {
-	log.Println("Tearing down VM")
-
-	err := orch.stopVm(vmID)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func SetupCloseHandler(orch *Orchestrator, vmID string) {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		fmt.Println("\r- Ctrl+C pressed in Terminal")
-		err := tearDownVM(orch, vmID)
-		if err != nil {
-			log.Printf("err: %v\n", err)
-			os.Exit(1)
-		}
-		os.Exit(0)
-	}()
 }
 
 func getSnapKey(vmID string) string {
 	return "demo-snap" + vmID
+}
+
+func bootVMfromSnapshot(networkManager *networking.NetworkManager, vmID, containerSnapPath string) error {
+	ctx := context.Background()
+
+	socketFile := "/tmp/firecracker.socket"
+	ip, ipNet, err := net.ParseCIDR(networkManager.GetConfig(vmID).GetContainerCIDR())
+	if err != nil {
+		return fmt.Errorf("failed to parse CIDR: %w", err)
+	}
+	networkInterface := sdk.NetworkInterface{
+		StaticConfiguration: &sdk.StaticNetworkConfiguration{
+			MacAddress:  macAddress,
+			HostDevName: hostDevName,
+			IPConfiguration: &sdk.IPConfiguration{
+				IPAddr: net.IPNet{
+					IP:   ip,
+					Mask: ipNet.Mask,
+				},
+				Gateway:     net.ParseIP(networkManager.GetConfig(vmID).GetGatewayIP()),
+				Nameservers: []string{"8.8.8.8"},
+			},
+		},
+	}
+	cfg := sdk.Config{
+		SocketPath: socketFile,
+		NetworkInterfaces: []sdk.NetworkInterface{
+			networkInterface,
+		},
+		VMID: vmID,
+		NetNS: networkManager.GetConfig(vmID).GetNamespacePath(),
+	}
+
+	cmd := sdk.VMCommandBuilder{}.WithSocketPath(socketFile).WithBin("/usr/local/bin/firecracker").Build(ctx)
+	m, err := sdk.NewMachine(ctx, cfg, sdk.WithProcessRunner(cmd),
+		sdk.WithSnapshot("/tmp/mem_file", "/tmp/snapshot_file", containerSnapPath,
+			func(c *sdk.SnapshotConfig) { c.ResumeVM = true }))
+	if err != nil {
+		return fmt.Errorf("creating new firecracker machine: %w", err)
+	}
+
+	err = m.Start(ctx)
+	if err != nil {
+		return fmt.Errorf("starting new firecracker machine: %w", err)
+	}
+
+	fmt.Printf("VM available at IP: %s\n", networkManager.GetConfig(vmID).GetCloneIP())
+	fmt.Println("Press the Enter Key to stop anytime")
+	fmt.Scanln()
+
+	if err := m.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := m.StopVMM(); err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
 }
